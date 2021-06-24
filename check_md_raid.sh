18b1d8bedf7db25c155979ce8cf463a0be8d784d
rincewind <rincewind@vulgrim.de>
2021-06-25 01:11:58 +0200

    Make shellcheck happy

--- check_md_raid.sh
+++ check_md_raid.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # Get count of raid arrays
-RAID_DEVICES=`grep ^md -c /proc/mdstat`
+RAID_DEVICES=$(grep ^md -c /proc/mdstat)
 
 # Get count of degraded arrays
-RAID_STATUS=`grep "\[.*_.*\]" /proc/mdstat -c`
+RAID_STATUS=$(grep "\[.*_.*\]" /proc/mdstat -c)
 
 # Is an array currently recovering, get percentage of recovery
-RAID_RECOVER=`grep recovery /proc/mdstat | awk '{print $4}'`
+RAID_RECOVER=$(grep recovery /proc/mdstat | awk '{print $4}')
 
 # Is an array currently resyncing, get percentage of resync
 
-RAID_RESYNC=`grep resync /proc/mdstat | awk '{print $4}'`
+RAID_RESYNC=$(grep resync /proc/mdstat | awk '{print $4}')
 
-RAID_ARRAY=`awk '/md[1-9]/{for (i=1;i<=NF;++i) if ($i~/md[1-2]/)print $i}' /proc/mdstat |xargs`
-RAID_DISKS=`awk '/sd[a-z]/{for (i=1;i<=NF;++i) if ($i~/sd[a-z]/)print $i}' /proc/mdstat |xargs`
-DISKS_STATUS=`grep algorithm  /proc/mdstat|awk '{print $12}'`
+RAID_ARRAY=$(awk '/md[1-9]/{for (i=1;i<=NF;++i) if ($i~/md[1-2]/)print $i}' /proc/mdstat |xargs)
+RAID_DISKS=$(awk '/sd[a-z]/{for (i=1;i<=NF;++i) if ($i~/sd[a-z]/)print $i}' /proc/mdstat |xargs)
+DISKS_STATUS=$(grep algorithm  /proc/mdstat|awk '{print $12}')
 
 # Check raid status
 # RAID recovers --> Warning
-if [[ $RAID_RECOVER ]]; then
+if [[ "$RAID_RECOVER" ]]; then
         STATUS="WARNING - Checked $RAID_DEVICES arrays $RAID_ARRAY, recovering : $RAID_RECOVER"
         EXIT=1
 # RAID resync --> Warning
-elif [[ $RAID_RESYNC ]]; then
+elif [[ "$RAID_RESYNC" ]]; then
         STATUS="WARNING - Checked $RAID_DEVICES arrays $RAID_ARRAY., resyncing : $RAID_RESYNC"
         EXIT=1
 # RAID ok
-elif [[ $RAID_STATUS  == "0" ]]; then
+elif [[ "$RAID_STATUS"  == "0" ]]; then
         STATUS="OK - Checked $RAID_DEVICES arrays $RAID_ARRAY."
         EXIT=0
 # All else critical, better save than sorry
